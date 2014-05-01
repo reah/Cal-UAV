371c9c7fa0ec0006e2f28aed916ebb54ca7733d0
Reah Miyara <reahmiyara@berkeley.edu>
2014-05-01 14:57:25 -0700

    Jquery and other tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,12 @@
-$(document).ready(function ( ){
-    
-    $('img.logo').valueOf();
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+$(document).ready(function() {
+	var body = $("html, body");
+	$("#about").click(function() {
+		body.animate({scrollTop: $("#mid2").offset().top}, 1000); 
+	});
+	$("#team").click(function() {
+		body.animate({scrollTop: $("#mid3").offset().top}, 1000);
+	});
+	$("#contact").click(function() {
+		body.animate({scrollTop: $("#mid5").offset().top}, 1000);
+	});
 });
